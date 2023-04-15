1557cc25faadcd5c734af49ecef6b848d81de0fa
maruk0chan <marco.chan@penqe.com>
2023-04-15 17:40:35 +0900

    finish a demo structure form

--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -1,13 +1,33 @@
 import React from 'react'
-import AutoCompleteInput from './AutoCompleteInput';
-import MultipleOptionsInput from './MultipleOptionsInput';
+import AutoCompleteInput from './AutoCompleteInput'
+import MultipleOptionsInput from './MultipleOptionsInput'
 
 const SearchForm = () => {
   return (
     <div className="border-double border-4 border-sky-500">
-      This is a search form.
-      <AutoCompleteInput/>
-      <MultipleOptionsInput/>
+      Search Condition(s)
+      <AutoCompleteInput />
+      <MultipleOptionsInput />
+      <div>
+        <label htmlFor="area">Choose areas* (e.g. Ginza, Asakusa)</label>
+        <MultipleOptionsInput />
+      </div>
+      <div>
+        <label htmlFor="budget">Budget (¥)*</label>
+        <MultipleOptionsInput />
+      </div>
+      <div>
+        <label htmlFor="count">Showing no. of results</label>
+        <AutoCompleteInput />
+      </div>
+      <div>
+        <label htmlFor="capacity">Party Capacity (more than)</label>
+        <input type="number" />
+      </div>
+      <div>
+        <button>Search</button>
+        <button>Copy</button>
+      </div>
     </div>
   )
 }
