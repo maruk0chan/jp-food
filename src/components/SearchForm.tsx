0f6f32920ec9755551683a8e77beae7447712342
maruk0chan <marco.chan@penqe.com>
2023-04-16 10:30:14 +0900

    add proxy cors

--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -1,16 +1,42 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import AutoCompleteInput from './AutoCompleteInput'
 import MultipleOptionsInput from './MultipleOptionsInput'
 
+const url =
+  'https://corsproxy.io/?' +
+  encodeURIComponent(
+    'https://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=bfa93fda871977e1&small_area=X021,X025,x045&count=20&party_capacity=50&format=json'
+  )
+
 const SearchForm = () => {
+  const [data, setData] = useState()
+
+  useEffect(() => {
+    // fetch data
+    const dataFetch = async () => {
+      const data = await (await fetch(url)).json()
+
+      // set state when the data received
+      setData(data)
+    }
+
+    dataFetch()
+  }, [])
+
   return (
     <div className="border-double border-4 border-sky-500 space-y-4">
       <div>Search Conditions</div>
       <div>
-        <MultipleOptionsInput title="Areas" placeholder="e.g. Ginza, Asakusa"/>
+        <MultipleOptionsInput
+          title="Areas"
+          placeholder="e.g. Ginza"
+        />
       </div>
       <div>
-        <MultipleOptionsInput title="Budget (¥)" placeholder="foobar"/>
+        <MultipleOptionsInput
+          title="Budget (¥)"und
+          placeholder="foobar"
+        />
       </div>
       <div>
         <AutoCompleteInput title="No. of results" />
