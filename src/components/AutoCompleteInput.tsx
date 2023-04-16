b0464a2c941c0657e7f003f3ade448ae1d670af4
maruk0chan <marco.chan@penqe.com>
2023-04-16 11:37:18 +0900

    finish autocompleteinput draft component

--- src/components/AutoCompleteInput.tsx
+++ src/components/AutoCompleteInput.tsx
@@ -1,15 +1,26 @@
 import { Autocomplete, TextField } from '@mui/material'
+import { useState } from 'react'
 
 interface AutocompleteInputProps {
   title: string
 }
 
 const AutocompleteInput = ({ title }: AutocompleteInputProps) => {
-  const options = ['The Godfather', 'Pulp Fiction']
+  const options = ['Default 1', 'Option 2', 'Option 3', 'Option 4', 'Option 5']
+  const [value, setValue] = useState<string | null>(options[0]);
+  const [inputValue, setInputValue] = useState('');
 
   return (
     <Autocomplete
       disablePortal
+      value={value}
+      onChange={(event: any, newValue: string | null) => {
+        setValue(newValue);
+      }}
+      inputValue={inputValue}
+        onInputChange={(event, newInputValue) => {
+          setInputValue(newInputValue);
+        }}
       id="combo-box-demo"
       options={options}
       sx={{ width: 300 }}
