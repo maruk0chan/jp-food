e6600d91f7db381a3eb2b369ee8316a81aa47f8b
maruk0chan <marco.chan@penqe.com>
2023-04-19 00:49:55 +0900

    switch redux to typescript

--- src/features/counter/Counter.tsx
+++ src/features/counter/Counter.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import { useSelector, useDispatch } from 'react-redux'
+import { useAppSelector, useAppDispatch } from '@/redux/hooks'
 import { decrement, increment } from './counterSlice'
 
 export function Counter() {
-  const count = useSelector((state) => state.counter.value)
-  const dispatch = useDispatch()
+  // The `state` arg is correctly typed as `RootState` already
+  const count = useAppSelector(state => state.counter.value)
+  const dispatch = useAppDispatch()
 
   return (
     <div>
